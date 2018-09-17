179601a6895233e9188eabf3614ded7d2b7ca379
Anthony Ngo <anthony.ngo180@gmail.com>
2018-09-17 14:40:47 -0700

    update

--- RobotCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ RobotCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -3,39 +3,115 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import java.util.Locale;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "TELEOP")
 public class TeleOpMain extends OpMode {
-  static DcMotor left, right, forklift;
-  static double leftp = 0, rightp = 0;
-  private TankDriveSystem drive = new TankDriveSystem();
+  private static DcMotor left, right, forklift;
+  private TankDrive drive = new TankDrive();
   private ArmSystem arm = new ArmSystem();
-  private static TeleOpMain instance = new TeleOpMain();
-  static Gamepad driver1 = instance.gamepad1;
-  static Gamepad driver2 = instance.gamepad2;
 
   @Override public void init() {
     left = hardwareMap.dcMotor.get("left");
     right = hardwareMap.dcMotor.get("right");
     forklift = hardwareMap.dcMotor.get("lift");
+    drive.init();
+    arm.init();
+  }
 
-    left.setDirection(DcMotor.Direction.REVERSE);
-    forklift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+  @Override
+  public void start() {
     drive.start();
     arm.start();
   }
 
   @Override
   public void loop() {
-    telemetry.addData("TankSystem left", String.format(Locale.US, "%.2f", leftp));
-    telemetry.addData("TankSystem right", String.format(Locale.US, "%.2f", rightp));
+    drive.report();
+    arm.report();
   }
 
   @Override
   public void stop() {
     drive.interrupt();
     arm.interrupt();
+  }
+
+  class TankDrive extends Thread implements SubSystem {
+    double leftY, rightY;
+    double multiplier = 1.0;
+
+    @Override public void init() {
+      left.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override public void run() {
+      while(!isInterrupted()) {
+        leftY = scaleInput(-gamepad1.left_stick_y);
+        rightY = scaleInput(gamepad1.right_stick_y);
+
+        leftY = Range.clip(leftY, -1, 1);
+        rightY = Range.clip(rightY, -1, 1);
+        leftY *= multiplier;
+        rightY *= multiplier;
+        left.setPower(leftY);
+        right.setPower(rightY);
+      }
+    }
+
+    private double scaleInput(double dVal)  {
+      double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+          0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+      // get the corresponding index for the scaleInput array.
+      int index = (int) (dVal * 16.0);
+
+      // index should be positive.
+      if (index < 0) {
+        index = -index;
+      }
+
+      // index cannot exceed size of array minus 1.
+      if (index > 16) {
+        index = 16;
+      }
+
+      // get value from the array.
+      // return scaled value.
+      return (dVal < 0) ? -scaleArray[index] : scaleArray[index];
+    }
+
+    @Override public void report() {
+      telemetry.addData("TankDrive", "left %d right %d", leftY, rightY);
+    }
+  }
+
+  class ArmSystem extends Thread implements SubSystem {
+    int minTick = 0, tick = 0;
+    double raisePower = 0.5;
+    double inches = 12.0;
+    int maxTick = (int) (inches * 25.4);
+
+    @Override public void init() {
+      forklift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override public void run() {
+      while(!isInterrupted()) {
+        if(gamepad2.y && tick < maxTick) {
+          forklift.setPower(raisePower);
+          tick++;
+        } else if(gamepad2.a && tick > minTick) {
+          forklift.setPower(-raisePower);
+          tick--;
+        } else {
+          forklift.setPower(0);
+        }
+      }
+    }
+
+    @Override public void report() {
+      telemetry.addData("ArmSystem", "power %d tick %d", raisePower, tick);
+    }
   }
 }
